d462e4ff35f7c62cb9b38df0f0d0bb6b7ac8d529
DarkSeid <38841221+iTzSkar@users.noreply.github.com>
2018-12-14 00:07:35 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const moment = require('moment');
-const prefix = '#'
+const prefix = '#'///hi
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
  
